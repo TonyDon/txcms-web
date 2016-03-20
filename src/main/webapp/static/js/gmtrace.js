6043c02bb7a82ff15cc4516b4e40ba95289ead0b
TonyDon <hongdeviltxd@163.com>
2016-03-20 22:40:09 +0800

    update

--- src/main/webapp/static/js/gmtrace.js
+++ src/main/webapp/static/js/gmtrace.js
@@ -1,1 +1,8 @@
 ;
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?9f500a4f9d80fd5ee3ce3293d0d896d5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
